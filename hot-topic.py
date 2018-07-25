8ee2f2be0f31234eed6d29b6c14a0ddfc928969d
hugo7650 <31364658+hugo7650@users.noreply.github.com>
2018-07-25 09:46:34 +0800

    update save result

--- hot-topic.py
+++ hot-topic.py
@@ -1,10 +1,16 @@
 import os
-
 from Spiders import *
 from Prasers import MainRanker
+from getarticles import get_articles
+
+def saveresult(result):
+    with open('result.txt', 'w') as f:
+        for i in result:
+            f.write(str(i)+'\n')
 
 if __name__ == '__main__':
     area_specifics_words = ['student', 'children', 'school', 'safe', 'educ', 'teacher', 'learn', ]
+
 
     # crawl pages from data.gov
     #spider = DataGovSpider()
@@ -18,13 +24,15 @@ if __name__ == '__main__':
     #spider = TheGuardianSpider()
     #spider.start('./data/TheGuardian')
 
-    ranker = MainRanker('./data/crawled', 748, True)
-    #print(ranker.simple_BOW_rank())
-    #print(ranker.BOW_stem_stop_rank())
-    #print(ranker.POS_rank('NP'))
-    #print(ranker.ngrams_rank())
-    print(ranker.tfidf_POS_rank(['NN', 'NP', 'NNS'],  myhottest=50, stemmer_name='Snowball', para='"english"', to_remove=area_specifics_words))
-    #print(ranker.tfidf_rank(myhottest=30, stemmer_name='Snowball', para='"english"', to_remove=area_specifics_words))
-
+    ranker = MainRanker('all', 748, True)
+    #word_freq = ranker.simple_BOW_rank()
+    #word_freq = ranker.BOW_stem_stop_rank()
+    #word_freq = ranker.POS_rank('NNP')
+    #word_freq = ranker.ngrams_rank()
+    #word_freq = ranker.tfidf_rank(myhottest=30, stemmer_name='Snowball', para='"english"', to_remove=area_specifics_words)
+    word_freq = ranker.tfidf_POS_rank(['NN', 'NP', 'NNS'],  myhottest=50, stemmer_name='Snowball', para='"english"', to_remove=area_specifics_words)
+    result = get_articles(word_freq)
     
+    saveresult(result)
     print('Program ' + os.path.basename(__file__) + ' ends sucessfully')
+    
